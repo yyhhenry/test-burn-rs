a230294fbb9bd6c141956d8c4a9aa16674d03942
yyhhenry <yyhhenry@foxmail.com>
2023-10-21 23:55:12 +0800

    fix: infer

--- src/bin/infer.rs
+++ src/bin/infer.rs
@@ -30,7 +30,7 @@ fn get_image_path() -> String {
     }
     args[1].to_owned()
 }
-fn build_model() -> Model<WgpuBackend> {
+fn build_model<B: Backend>() -> Model<B> {
     // After training, we can load the model and use it to make predictions.
     let model_bytes = include_bytes!("../../tmp/model.bin");
 
@@ -38,7 +38,7 @@ fn build_model() -> Model<WgpuBackend> {
         .load(model_bytes.to_vec())
         .expect("Failed to load model");
 
-    Model::<WgpuBackend>::new().load_record(record)
+    Model::<B>::new().load_record(record)
 }
 fn get_max_probability(output: &[f32; 10]) -> (usize, f32) {
     let (number, probability) = output
@@ -53,16 +53,38 @@ fn read_grayscale_image(path: &str) -> [u8; 28 * 28] {
     let image = imageops::resize(&image, 28, 28, Gaussian);
     image.to_vec().try_into().unwrap()
 }
+fn infer_image_and_print<B: Backend>(model: &Model<B>, image_path: &str) {
+    println!("Inferring {}", image_path);
+    let image = read_grayscale_image(image_path);
+    let output = infer::<B>(&model, &image);
+    println!("Output: {:?}", output);
+    let (number, probability) = get_max_probability(&output);
+    println!("(number, probability) = ({number}, {probability})");
+}
 
 fn main() {
     let image_path = get_image_path();
-    let model = build_model();
+    let model = build_model::<WgpuBackend>();
 
-    let image = read_grayscale_image(&image_path);
+    let accept_extensions = ["png", "jpg", "jpeg"];
 
-    let output = infer::<WgpuBackend>(&model, &image);
-    println!("Output: {:?}", output);
-
-    let (number, probability) = get_max_probability(&output);
-    println!("(number, probability) = ({number}, {probability})");
+    let path = std::path::Path::new(&image_path);
+    if !path.exists() {
+        println!("{} does not exist", image_path);
+        std::process::exit(1);
+    }
+    if path.is_dir() {
+        println!("{} is a directory", image_path);
+        let paths = std::fs::read_dir(image_path).unwrap();
+        for path in paths {
+            let path = path.unwrap().path();
+            let ext = path.extension().unwrap().to_str().unwrap();
+            if path.is_file() && accept_extensions.contains(&ext) {
+                let path = path.to_str().unwrap();
+                infer_image_and_print(&model, path);
+            }
+        }
+    } else {
+        infer_image_and_print(&model, &image_path);
+    }
 }
